9bc5bce7e74978869424a6054edf5400c4b8df9a
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-20 20:19:26 +0300

    Create Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+    "Hey how are you",
+    "Good evening",
+    "Welcome to TBM company",
+    "Welcome to your developer",
+    "Have a lovely day"
+]
+
+greetings.each do |message|
+  Message.create(content: message)
+end
